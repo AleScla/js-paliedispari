674d481f99a2d59ef2e8853967a0a676cece9db7
AleScla <scla.alessandro@gmail.com>
2024-07-19 16:16:29 +0200

    tolti i console.log rimosse due variabili come da consiglio di Alessandro Inseriti commenti

--- pari e dispari/js/scripts.js
+++ pari e dispari/js/scripts.js
@@ -4,25 +4,38 @@
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
+
+
+// variabili globali
+let somma 
+const form = document.querySelector('form')
+const user = document.getElementById('utente')
+const pc = document.getElementById('pc')
+
+
 // funzioni
+
+
 function calcRandom (min, max){
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
-let somma 
 
-function sum (){
-    let min = 1;
-    let max = 5;
-    let pcnumber = calcRandom(min, max);
-    console.log('Pc number è:', pcnumber);
+
+
+function sum (minSomma, maxSomma){
+    // calcolo la somma di due numeri random (in questo caso tra 1 , 5)
+    let pcnumber = calcRandom(minSomma, maxSomma);
+    // stampo il numero in html
     pc.innerHTML = 'Pc ha scelto:  ' + pcnumber; 
+    // creo una variabile per la scelta dell'utente pari o dispari
     let userchoice = document.getElementById('scelta-utente').value;
-    console.log(userchoice);
+    // creo una variabile per il numero scelto dall'utente
     let usernumber = parseInt(document.getElementById('scelta-numero').value);
-    console.log(usernumber);
+    // stampo le scelte dell'utente in pagina
     user.innerHTML = 'Le tue scelte sono: ' + userchoice + ' e ' + usernumber; 
-    somma = pcnumber + usernumber
-
+    // sommo i numeri dell'utente e del pc 
+    somma = pcnumber + usernumber;
+    // metto le condizioni di vittoria
     if(somma % 2 == 0 && userchoice == 'pari') {
         console.log('Pari! User win');
         return document.getElementById('risultato').innerHTML = ('Pari! User win');
@@ -39,12 +52,10 @@ function sum (){
     }
 }
 
-const form = document.querySelector('form')
-const user = document.getElementById('utente')
-const pc = document.getElementById('pc')
+// evento sul form per raccogliere i dati dell'utente
 form.addEventListener('submit', function winner(event){
     event.preventDefault();
-    sum()
+    sum(1, 5);
     document.getElementById('estrazione').innerHTML = ('La somma dei due numeri è: ' + somma);
     
 })
